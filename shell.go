e483085edd306d8266743c18aa8ba47ee6533af8
bingoohuang <bingoo.huang@gmail.com>
2017-12-06 20:35:15 +0800

    shell execute

--- shell.go
+++ shell.go
@@ -2,39 +2,15 @@ package main
 
 import (
 	"os/exec"
-	"io/ioutil"
-	"os"
 	"fmt"
 	"io"
 	"bytes"
-	"time"
 	"log"
+	"time"
 )
 
-func ExecuteCommands(cmds string) (string, string) {
-	pwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	file, err := ioutil.TempFile(pwd, "TempShellFile")
-	if err != nil {
-		return "", err.Error()
-	}
-
-	fileName := file.Name()
-	fmt.Println("TempShellFile:", fileName)
-	//defer os.Remove(fileName)
-
-	file.WriteString("#!/usr/bin/env bash\n\n")
-	file.WriteString(cmds)
-	file.Close()
-
-	cmd := exec.Command("bash", "-c", fileName)
-	//output, err := cmd.Output()
-
-	//return string(output), ""
+func ExecuteCommands(cmds string, timeoutSeconds time.Duration) (string, string) {
+	cmd := exec.Command("bash", "-c", cmds)
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
@@ -45,15 +21,18 @@ func ExecuteCommands(cmds string) (string, string) {
 	if err != nil {
 		return "", err.Error()
 	}
+
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("pid:", cmd.Process.Pid)
 	//
 	chStdout := readOut(stdout)
 	chStderr := readOut(stderr)
 
 	quit := make(chan bool)
-	time.AfterFunc(3*time.Second, func() { quit <- true })
+	time.AfterFunc(timeoutSeconds, func() { quit <- true })
 
 	var bufferStdout bytes.Buffer
 	var bufferStderr bytes.Buffer
@@ -63,21 +42,21 @@ LOOP:
 		select {
 		case s, ok := <-chStdout:
 			if !ok {
+				cmd.Process.Kill()
 				break LOOP
 			}
-			fmt.Print(s)
 			bufferStdout.WriteString(s)
 		case s, ok := <-chStderr:
 			if !ok {
 				break LOOP
 			}
-			fmt.Print(s)
 			bufferStderr.WriteString(s)
 		case <-quit:
 			cmd.Process.Kill()
 		}
 	}
-	fmt.Println("all done!")
+
+	cmd.Wait()
 
 	return bufferStdout.String(), bufferStderr.String()
 }
@@ -95,7 +74,6 @@ func readOut(closer io.ReadCloser) chan string {
 				break
 			}
 		}
-		fmt.Println("Goroutine finished")
 		close(ch)
 	}()
 
@@ -103,7 +81,7 @@ func readOut(closer io.ReadCloser) chan string {
 }
 
 func main() {
-	out, err := ExecuteCommands("ls\n")
+	out, err := ExecuteCommands("ls", 3*time.Second)
 	fmt.Print(out)
 	fmt.Print(err)
 }
