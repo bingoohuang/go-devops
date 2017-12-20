041542adb18fea329a0f14106c015b1f08b127ae
bingoo <bingoo.huang@gmail.com>
2017-12-20 11:15:38 +0800

    deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,31 @@ targetHost=$1
 deployName=go-devops
 
 rm -fr $deployName.linux.bin $deployName.linux.bin.bz2
+./gobin.sh
 env GOOS=linux GOARCH=amd64 go build -o $deployName.linux.bin
 bzip2 $deployName.linux.bin
-rsync -avz --human-readable --progress -e "ssh -p 22" ./$deployName.linux.bin.bz2 $targetHost:./
+rsync -avz --human-readable --progress -e "ssh -p 22" ./$deployName.linux.bin.bz2 $targetHost:.
 #scp ./$deployName.linux.bin.bz2 $targetHost:./
-scp ./deploy-agent.sh $targetHost:./
+scp ./deploy-agent.sh $targetHost:.
+
 ssh -tt $targetHost "bash -s" << eeooff
+mkdir -p ./app/$deployName/
+cd ./app/$deployName/
+mv ~/$deployName.linux.bin.bz2 .
+mv ~/deploy-agent.sh .
+ps -ef|grep $deployName|grep -v grep|awk '{print \$2}'|xargs -r kill -9
 chmod +x ./deploy-agent.sh
-./deploy-agent.sh $deployName app01 app
-rm -f ./deploy-agent.sh
+./deploy-agent.sh $deployName app01 app/$deployName
+./deploy-agent.sh $deployName app02 app/$deployName
+./deploy-agent.sh $deployName cp01 app/$deployName
+./deploy-agent.sh $deployName cp02 app/$deployName
+./deploy-agent.sh $deployName ino01 app/$deployName
+./deploy-agent.sh $deployName ino02 app/$deployName
+./deploy-agent.sh $deployName smc01 app/$deployName
+./deploy-agent.sh $deployName bap01 app/$deployName
+./deploy-agent.sh $deployName bam01 app/$deployName
+rm -f $deployName.linux.bin
+bzip2 -d $deployName.linux.bin.bz2
+nohup ./$deployName.linux.bin &
 exit
 eeooff
--- res/index.js
+++ res/index.js
@@ -6,8 +6,8 @@
             var machines = $('#machines')
             var machinesHtml = '<table>' +
                 '<tr><td>Name</td><td>State</td><td>Hostname</td><td>IP</td>' +
-                '</td><td>OS</td><td>Total Memory</td><td>Available Memory</td>' +
-                '<td>Memory Used</td><td>Cores</td><td>Total Disk</td><td>Free Disk</td><td>Disk Used</td></tr>'
+                '</td><td>OS</td><td>Cores</td><td>Total Memory</td><td>Available Memory</td>' +
+                '<td>Memory Used</td><td>Total Disk</td><td>Free Disk</td><td>Disk Used</td></tr>'
             if (content && content.length) {
                 for (var j = 0; j < content.length; j++) {
                     var machineResult = content[j]
@@ -18,10 +18,10 @@
                         + '<td>' + hardwareInfo.Hostname + '</td>'
                         + '<td>' + (hardwareInfo.Ips && hardwareInfo.Ips.length > 0 ? hardwareInfo.Ips.join(', ') : '') + '</td>'
                         + '<td>' + hardwareInfo.OS + '</td>'
+                        + '<td>' + hardwareInfo.Cores + '</td>'
                         + '<td>' + hardwareInfo.HumanizedTotalMemory + '</td>'
                         + '<td>' + hardwareInfo.HumanizedAvailableMemory + '</td>'
                         + '<td>' + new Number(hardwareInfo.MemoryUsedPercent).toFixed(2) + '%</td>'
-                        + '<td>' + hardwareInfo.Cores + '</td>'
                         + '<td>' + hardwareInfo.HumanizedTotalDisk + '</td>'
                         + '<td>' + hardwareInfo.HumanizedFreeDisk + '</td>'
                         + '<td>' + new Number(hardwareInfo.DiskUsedPercent).toFixed(2) + '%</td>'
@@ -36,6 +36,4 @@
             alert(jqXHR.responseText + "\nStatus: " + textStatus + "\nError: " + errorThrown)
         }
     })
-
-
 })()
