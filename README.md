0a8f2f519b16ab2a2a1ebd1978a7d5366da4d430
bingoo <bingoo.huang@gmail.com>
2017-12-20 15:19:41 +0800

    favicon.ico

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-devops
 devops based on go
+
+## Utils
+1. [online png to ico](https://cloudconvert.com/png-to-ico)
--- http.go
+++ http.go
@@ -10,7 +10,9 @@ func StartHttpSever() {
 	r := mux.NewRouter()
 	r.HandleFunc(contextPath+"/log/{logger}/{timestampFrom}/{timestampTo}", FindHandleLogsBetweenTimestamps)
 	r.HandleFunc(contextPath+"/machines", HandleMachines)
+	r.HandleFunc(contextPath+"/favicon.ico", HandleFavicon)
 	r.HandleFunc(contextPath+"/", HandleHome)
+
 	http.Handle(contextPath+"/", r)
 
 	fmt.Println("start to listen at ", httpPort)
