0e3e6661525fe7e4623b38d62b60d4cdc2d87b68
bingoohuang <bingoo.huang@gmail.com>
2018-01-01 22:10:35 +0800

    tail queue prepare

---
+++ tail.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"os/exec"
+	"bufio"
+	"io"
+	"log"
+	"sync"
+)
+
+var tailMap sync.Map
+
+type TailInfo struct {
+	logBlocks []string
+}
+
+func tail(logFile string, seq int) string {
+	tailInfo, ok := tailMap.Load(logFile)
+	if !ok {
+		newTailInfo := TailInfo{
+
+		}
+		tailInfo, loaded := tailMap.LoadOrStore(logFile, &newTailInfo)
+		if !loaded {
+			startTail(logFile, tailInfo)
+		}
+	}
+}
+
+func startTail(logFile string, tailInfo *TailInfo) {
+	cmd := exec.Command("tail", "-F", logFile)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stdout.Close()
+
+	reader := bufio.NewReader(stdout)
+	if err := cmd.Start(); err != nil {
+		log.Fatal("Buffer Error:", err)
+	}
+
+	dataChan := make(chan []byte)
+	go readInput(reader, dataChan)
+}
+
+func readInput(reader *bufio.Reader, dataChan chan []byte) {
+	tmp := make([]byte, 10240)
+	for {
+		length, err := reader.Read(tmp)
+		if err != nil && err != io.EOF {
+			log.Println("read error", err.Error())
+			break
+		}
+
+		dataChan <- tmp[0:length]
+	}
+}
