aeae3553db4cd6ebc8194a769623e1c15df8c06b
bingoo <bingoo.huang@gmail.com>
2018-06-23 21:28:41 +0800

    upx and deploy fast mode

--- deploy-agent.sh
+++ deploy-agent.sh
@@ -5,14 +5,12 @@ targetHost=$2
 targetPath=$3
 rpcPort=${4:-6979}
 startHttp=${5:-true}
-scp ./$deployName.linux.bin.bz2 $targetHost:.
+scp ./$deployName.linux.bin $targetHost:.
 ssh -tt $targetHost "bash -s" << eeooff
 mkdir -p $targetPath/
-mv $deployName.linux.bin.bz2 $targetPath/
+mv -f $deployName.linux.bin $targetPath/
 cd $targetPath/
 ps -ef|grep $deployName|grep -v grep|awk '{print \$2}'|xargs -r kill -9
-rm -f $deployName.linux.bin
-bzip2 -d $deployName.linux.bin.bz2
 nohup ./$deployName.linux.bin -rpcPort=$rpcPort -startHttp=$startHttp 2>&1 >> nohup.out &
 exit
 eeooff
